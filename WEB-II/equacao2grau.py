04581f0089c2265ee89784ede4f8b0631c504c10
Andre Luiz <andrejarczewski@gmail.com>
2026-02-27 12:07:03 -0300

    Equacao2grau

---
+++ WEB-II/equacao2grau.py
@@ -0,0 +1,18 @@
+print ("Equação de Segundo Grau")
+a = float(input("Digite valo de a:"))
+b = float(input("Digite valo de b:"))
+c = float(input("Digite valo de c:"))
+
+
+delta = b*b-(4*a*c)
+
+if delta > 0:
+    x1 = (-b + pow(delta, 0.5)) / (2*a)
+    x2 = (-b - pow(delta, 0.5)) / (2*a)
+    print(f"x1 = {x1:.2f}")
+    print(f"x2 = {x2:.2f}")
+elif delta == 0:
+    x= (-b)/2*a
+    print ("x=", x, "(Delta igual a zero)")
+else:
+    print ("Erro: Delta < 0")
